74835d045524faa1fb68d56f490c6870cb7fcf41
Pedro Quiroga <pitterbck@gmail.com>
2021-07-31 22:46:48 -0300

    Arreglado navbar, footer. Finalizado estilo modal y bg-gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
       e.preventDefault();
     });
   });
+
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+  document.body.classList.add("dark-theme");
+} else {
+  document.body.classList.remove("dark-theme");
+}
